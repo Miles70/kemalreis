bc6759b561798e3daa018d2bf60e853a34e8b7e5
Miles70 <Selimyalcinner@gmail.com>
2026-07-13 18:10:12 +0300

    Add frontend crypto payment constants

---
+++ src/config/cryptoPayment.js
@@ -0,0 +1,14 @@
+export const ERC20_TRANSFER_ABI = [
+  {
+    type: "function",
+    name: "transfer",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "recipient", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [{ name: "", type: "bool" }],
+  },
+];
+
+export const BSC_EXPLORER_TRANSACTION_URL = "https://bscscan.com/tx";
